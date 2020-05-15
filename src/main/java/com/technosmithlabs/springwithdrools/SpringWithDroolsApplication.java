66f3056bb6fa84575db1684d2f5f1a928d476873
technosmithlabs <email@anshulgautam.in>
2020-05-15 21:45:28 +0530

    first commit

---
+++ src/main/java/com/technosmithlabs/springwithdrools/SpringWithDroolsApplication.java
@@ -0,0 +1,13 @@
+package com.technosmithlabs.springwithdrools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWithDroolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWithDroolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/technosmithlabs/springwithdrools/SpringWithDroolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technosmithlabs.springwithdrools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWithDroolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
