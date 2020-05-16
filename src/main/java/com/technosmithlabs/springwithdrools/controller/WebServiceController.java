b014be8291f39f557e671fff3f9b9c633929a36c
technosmithlabs <email@anshulgautam.in>
2020-05-16 16:01:56 +0530

    working code

--- src/main/java/com/technosmithlabs/springwithdrools/controller/WebServiceController.java
+++ src/main/java/com/technosmithlabs/springwithdrools/controller/WebServiceController.java
@@ -1,21 +1,14 @@
 package com.technosmithlabs.springwithdrools.controller;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-
-import org.drools.decisiontable.InputType;
-import org.drools.decisiontable.SpreadsheetCompiler;
 import org.kie.api.runtime.KieSession;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.technosmithlabs.springwithdrools.model.Request;
 import com.technosmithlabs.springwithdrools.model.Response;
+import com.technosmithlabs.springwithdrools.util.CommonUtil;
 
 @RestController
 public class WebServiceController {
@@ -23,19 +16,12 @@ public class WebServiceController {
 	@Autowired
 	private KieSession session;
 
-	@Autowired
-	private Response responseObject;
-
 	@RequestMapping(method = RequestMethod.POST, value = "/status")
-	public Response evalStatus(@RequestBody Request requestObject) throws FileNotFoundException {
-		final InputStream is = new FileInputStream(new File(
-				"/Users/anshulgautam/anshul-workstation/projects/springbootws-with-drools-decisiontable/src/main/resources/drools/rules.xls"));
-		SpreadsheetCompiler comp = new SpreadsheetCompiler();
-		String drl = comp.compile(false, is, InputType.XLS);
-		System.out.println(drl);
-		System.out.println(requestObject.getFullName());
+	public Response evalStatus(Request requestObject) {
+		final Response responseObject = new Response();
 		session.insert(requestObject);
 		session.setGlobal("response", responseObject);
+		session.setGlobal("util", new CommonUtil());
 		session.fireAllRules();
 		return responseObject;
 	}
--- src/main/java/com/technosmithlabs/springwithdrools/util/CommonUtil.java
+++ src/main/java/com/technosmithlabs/springwithdrools/util/CommonUtil.java
@@ -1,8 +1,15 @@
 package com.technosmithlabs.springwithdrools.util;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+
+import org.drools.decisiontable.InputType;
+import org.drools.decisiontable.SpreadsheetCompiler;
 
 public class CommonUtil {
 
@@ -10,8 +17,17 @@ public class CommonUtil {
 
 	public static Long getAge(String dob) {
 		final LocalDate date = LocalDate.parse(dob, DATE_FORMATTER);
-		final Long age = ChronoUnit.DAYS.between(date, LocalDate.now());
+		final Long age = ChronoUnit.YEARS.between(date, LocalDate.now());
 		System.out.println("Age is: " + age);
 		return age;
+	}
+
+	public static String compileRulesExcelToDrl() throws FileNotFoundException {
+		final InputStream is = new FileInputStream(new File(
+				"/Users/anshulgautam/anshul-workstation/projects/springbootws-with-drools-decisiontable/src/main/resources/drools/rules.xls"));
+		SpreadsheetCompiler comp = new SpreadsheetCompiler();
+		String drl = comp.compile(false, is, InputType.XLS);
+		System.out.println(drl);
+		return drl;
 	}
 }
